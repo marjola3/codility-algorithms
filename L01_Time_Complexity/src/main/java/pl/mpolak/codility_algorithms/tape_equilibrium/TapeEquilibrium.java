52ad5daef9ba3aba567f46a0e0249f138962129a
marjola3 <marjola3@poczta.onet.pl>
2015-06-29 20:10:06 +0200

    solution

--- L01_Time_Complexity/src/main/java/pl/mpolak/codility_algorithms/tape_equilibrium/TapeEquilibrium.java
+++ L01_Time_Complexity/src/main/java/pl/mpolak/codility_algorithms/tape_equilibrium/TapeEquilibrium.java
@@ -6,6 +6,30 @@ package pl.mpolak.codility_algorithms.tape_equilibrium;
 public class TapeEquilibrium {
 
     public int solution(int[] A) {
-        return 1;
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+        }
+
+        int leftSum = 0;
+        int rightSum = 0;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 1; i < A.length; i++) {
+            leftSum += A[i - 1];
+            rightSum = sum -leftSum;
+
+            int substr = leftSum - rightSum;
+            if(substr < 0) {
+                substr = substr * -1;
+            }
+
+            if(substr < min) {
+                min = substr;
+            }
+
+        }
+
+        return min;
     }
 }
